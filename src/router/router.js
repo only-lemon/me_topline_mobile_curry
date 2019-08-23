900586056a2c9f7eebff3ab7afa65ed82f620cb5
cuicui <2865653500@qq.com>
2019-08-23 12:54:12 +0800

    初始化项目目录结构 下一步准备coding,,,

---
+++ src/router/router.js
@@ -0,0 +1,22 @@
+/**           配置路由规则模块
+ *
+ *    Author  : 崔文情
+ *    Date    : 2019/8/23 08:30 Fri
+ *    Email   : 2865653500@qq.com
+ *    Version : V1.0.0
+ */
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 创建路由管理器的对象router
+const router = new VueRouter({
+  // 配置me_topline_mobile_curry 路由规则
+  // routes: [
+  //   { path: '/' ,name:"/",}
+  // ]
+})
+
+export default router
