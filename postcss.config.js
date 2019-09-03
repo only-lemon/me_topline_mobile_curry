7ee8dddb0da7b9a891f9536874b50da4d4bcc8d6
cuicui <2865653500@qq.com>
2019-09-03 11:16:09 +0800

    对项目增加rem适配,,,接下来马上自己操作测试打包结果,,,把项目开发完毕后准备打包上线,,,打包后上线前要自己测试打包后的结果,,,确保打包后的结果没问题后再部署上线,,,再部署到线上,,,

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    // 把 px 转成 rem
+    'postcss-pxtorem': {
+      rootValue: 37.5,
+      propList: ['*']
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import '@/style/common_style.css'
 import { relativeTime } from '@/utility/dayjs'
 import fastClick from 'fastclick'
 
+// 导入此包的作用是: 设置媒体查询的HTML的font-size的基准值,不需要拿变量接收,,,一旦导入它内部会自动执行一段代码,,,
+import 'amfe-flexible/index.js'
+
 // 使用 fastClick 解决移动端 300ms 延迟问题
 fastClick.attach(document.body)
 
